eac0edb2525a779c4af7100f25bafff1b2fc7d0b
lj970926 <1783973490@qq.com>
2022-02-21 13:14:31 +0800

    async log

--- logger/log.cc
+++ logger/log.cc
@@ -7,11 +7,15 @@ Log* Log::GetInstance() {
   return &log;
 }
 
-void Log::init(const char* file_name, int log_buf_size, int max_lines, int max_queue_size) {
+void Log::Init(const char* file_name, int log_buf_size, int max_lines, int max_queue_size) {
   log_buf_ = new char[log_buf_size];
   
   if (max_queue_size > 0) {
-    is_async_mode = true;
-    
+    is_async_mode_ = true;
+    log_queue_ = new BlockQueue<string>(max_queue_size);
   }
+
+  log_buf_ = new char[log_buf_size];
+  memset(log_buf_, '\0', sizeof(char) * log_buf_size);
+  max_lines_ = max_lines;
 }
--- logger/log.h
+++ logger/log.h
@@ -1,10 +1,20 @@
+#pragma once
+
+#include <string>
+
+#include "block_queue.h"
+
+using std::string;
+
 class Log {
 public:
   Log* GetInstance();
-  void init(const char* file_name, int log_buf_size, int max_lines, int max_queue_size = 0);
+  void Init(const char* file_name, int log_buf_size, int max_lines, int max_queue_size = 0);
 private:
   Log();
   ~Log();
   char* log_buf_;
-  bool is_async_mode = false;
+  bool is_async_mode_ = false;
+  BlockQueue<string>* log_queue_;
+  int max_lines_;
 };
