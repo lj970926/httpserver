c94d510c40ce332f76e697c75661c12c59147405
lj970926 <1783973490@qq.com>
2022-02-21 14:53:49 +0800

    async log

--- logger/log.cc
+++ logger/log.cc
@@ -1,5 +1,8 @@
 #include "log.h"
 #include <string.h>
+#include <time.h>
+#include <pthread.h>
+
 
 
 Log* Log::GetInstance() {
@@ -13,9 +16,47 @@ void Log::Init(const char* file_name, int log_buf_size, int max_lines, int max_q
   if (max_queue_size > 0) {
     is_async_mode_ = true;
     log_queue_ = new BlockQueue<string>(max_queue_size);
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread_write_file, NULL);
   }
 
   log_buf_ = new char[log_buf_size];
   memset(log_buf_, '\0', sizeof(char) * log_buf_size);
   max_lines_ = max_lines;
+  char log_full_name[256] = {0};
+  time_t calendar_time = time(NULL);
+  struct tm* brok_time = localtime(&calendar_time);
+  const char* pos_slash = strrchr(file_name, '/');
+
+  if (!pos_slash) {
+    snprintf(log_full_name, 255, "%d_%02d_%02d_%s",
+             brok_time->tm_year + 1900,
+             brok_time->tm_mon + 1,
+             brok_time->tm_mday,
+             file_name);
+  } else {
+    strcpy(log_name_, pos_slash + 1);
+    strncpy(dir_name_, file_name, pos_slash - file_name + 1);
+    snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s",
+             dir_name_,
+             brok_time->tm_year + 1900,
+             brok_time->tm_mon + 1,
+             brok_time->tm_mday,
+             log_name_);
+  }
+  fp_ = fopen(log_full_name, "a");
+  return; fp_ != NULL;
+}
+
+void Log::thread_write_file(void *arg) {
+  GetInstance()->async_write_log();
+}
+
+void Log::async_write_log() {
+  string log;
+  while (log_queue_->Pop(log)) {
+    mutex_.Lock();
+    fputs(log.c_str(), fp_);
+    mutex_.Unlock();
+  }
 }
--- logger/log.h
+++ logger/log.h
@@ -2,19 +2,27 @@
 
 #include <string>
 
+#include <lock/locker.h>
 #include "block_queue.h"
 
 using std::string;
 
 class Log {
 public:
-  Log* GetInstance();
+  static Log* GetInstance();
   void Init(const char* file_name, int log_buf_size, int max_lines, int max_queue_size = 0);
+  static void* thread_write_file(void* arg);
 private:
+  void async_write_log();
   Log();
   ~Log();
   char* log_buf_;
   bool is_async_mode_ = false;
   BlockQueue<string>* log_queue_;
   int max_lines_;
+  FILE* fp_;
+
+  char dir_name_[128];
+  char log_name_[128];
+  Mutex mutex_;
 };
