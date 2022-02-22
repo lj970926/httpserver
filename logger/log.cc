7d32a0ad00c1defb2466637e7e40cd13fd730fdd
lj970926 <1783973490@qq.com>
2022-02-22 10:59:41 +0800

    finish logger

--- logger/log.cc
+++ logger/log.cc
@@ -2,8 +2,10 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 #include <time.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 
 
@@ -73,6 +75,8 @@ void Log::flush() {
 void Log::write(int level, const char* format, ...) {
   time_t now = time(NULL);
   struct tm* brok_tm = localtime(&now);
+  struct timeval time_val = {0, 0};
+  gettimeofday(&time_val, NULL);
 
   mutex_.Lock();
 
@@ -86,7 +90,59 @@ void Log::write(int level, const char* format, ...) {
     sprintf(prefix, "%d_%02d_%02d", brok_tm->tm_year + 1900, brok_tm->tm_mon + 1, brok_tm->tm_mday);
 
     if (today_ != brok_tm->tm_mday) {
-      snprintf()
+      snprintf(new_log, 255, "%s%s%s", dir_name_, prefix, log_name_);
+      today_ = brok_tm->tm_mday;
+      cur_lines_ = 0;
+    } else {
+      snprintf(new_log, 255, "%s%s%s.%d", dir_name_, prefix, log_name_, cur_lines_ / max_lines_);
     }
+
+    fp_ = fopen(new_log, "a");
+  }
+
+  mutex_.Unlock();
+
+  char level_str[16] = {0};
+  switch (level) {
+    case 0:
+      strcpy(level_str, "[debug]:");
+      break;
+    case 1:
+      strcpy(level_str, "[info]:");
+      break;
+    case 2:
+      strcpy(level_str, "[warn]:");
+      break;
+    case 3:
+      strcpy(level_str, "[error]:");
+      break;
+    default:
+      strcpy(level_str, "[info]:");
+      break;
+  }
+
+  mutex_.Lock();
+  int pre_len = sprintf(log_buf_, "%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
+          brok_tm->tm_year + 1900, brok_tm->tm_mon + 1,
+          brok_tm->tm_mday, brok_tm->tm_hour,
+          brok_tm->tm_sec, time_val.tv_usec, level_str);
+
+  va_list valist;
+  va_start(valist, format);
+  int cont_len = vsnprintf(log_buf_ + pre_len, 254 - pre_len, format, valist);
+  log_buf_[pre_len + cont_len] = '\n';
+  log_buf_[pre_len + cont_len + 1] = '\0';
+  va_end(valist);
+
+  string log_str = log_buf_;
+  ++cur_lines_;
+  mutex_.Unlock();
+
+  if (is_async_mode_ && !log_queue_->Full()) {
+    log_queue_->Push(log_str);
+  } else {
+    mutex_.Lock();
+    fputs(log_str.c_str(), fp_);
+    mutex_.Unlock();
   }
 }
