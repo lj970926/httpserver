d564e3b4d3840b485f25d09baf48fe9e566aa8eb
lj970926 <1783973490@qq.com>
2022-02-27 15:57:31 +0800

    add timer

---
+++ timer/timer.h
@@ -0,0 +1,14 @@
+//
+// Created by lijin on 2022/2/27.
+//
+
+#ifndef HTTPSERVER_TIMER_H
+#define HTTPSERVER_TIMER_H
+
+#include <arpa/inet.h>
+
+class Timer;
+class ClientData {
+  int sockfd;
+};
+#endif //HTTPSERVER_TIMER_H
